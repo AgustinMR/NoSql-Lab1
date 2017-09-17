7fb1586266ee957e14853bce0e220c325a404522
AgustinMR <agustinmr1995@gmail.com>
2017-09-17 15:09:50 -0300

    update === delete ?

--- src/main/java/com/nosql/dataAccess/DALUsuario.java
+++ src/main/java/com/nosql/dataAccess/DALUsuario.java
@@ -4,6 +4,7 @@ import com.nosql.entidades.Usuario;
 import com.orientechnologies.orient.core.db.ODatabase;
 import com.orientechnologies.orient.core.db.document.ODatabaseDocumentTx;
 import com.orientechnologies.orient.core.record.impl.ODocument;
+import com.orientechnologies.orient.core.sql.OCommandSQL;
 import com.orientechnologies.orient.core.sql.query.OSQLSynchQuery;
 
 import java.util.ArrayList;
@@ -44,18 +45,23 @@ public class DALUsuario implements IDALUsuario {
         try {
             ODatabaseDocumentTx database = new ODatabaseDocumentTx("plocal:/databases/lab1nosql");
             db = database.activateOnCurrentThread().open("admin", "admin");
-            ODocument u = getUsuario(db, usu.getUsername());
+            /*int recordsUpdated = db.command(
+                    new OCommandSQL("UPDATE Usuario SET username = ?, email = ?, nombre = ?, apellido = ?, password = ? WHERE @rid = ?")).
+                    execute(usu.getUsername(), usu.getEmail(), usu.getNombre(), usu.getApellido(), usu.getPassword(), usu.getRid());*/
 
-            u.field("nombre", usu.getNombre());
-            u.field("apellido", usu.getApellido());
-            u.field("email", usu.getEmail());
-            u.field("password", usu.getPassword());
-            u.field("username", usu.getUsername());
-
-            u.save();
-            db.commit();
+            ODocument doc = getUsuario(db, usu.getRid());
+            if(doc != null){
+                doc.field("username", usu.getUsername());
+                doc.field("email", usu.getEmail());
+                doc.field("nombre", usu.getNombre());
+                doc.field("apellido", usu.getApellido());
+                doc.field("password", usu.getPassword());
+                doc.save();
+            }
+            //db.commit();
             db.close();
             return true;
+            //return recordsUpdated == 1;
         } catch (Exception ex) {
             db.close();
             return false;
@@ -83,12 +89,28 @@ public class DALUsuario implements IDALUsuario {
     public Usuario getUsuario(String username) {
         ODatabaseDocumentTx database = new ODatabaseDocumentTx("plocal:/databases/lab1nosql");
         ODatabase db = database.activateOnCurrentThread().open("admin", "admin");
-        ODocument od = (ODocument) db.query(new OSQLSynchQuery("SELECT FROM Usuario WHERE username=" + username)).get(0);
-        return new Usuario(od.field("nombre"), od.field("apellido"), od.field("email"), od.field("password"), od.field("rid"));
+        OSQLSynchQuery<ODocument> query = new OSQLSynchQuery<ODocument>("SELECT FROM Usuario WHERE username = ?");
+        List<ODocument> result = database.command(query).execute(username);
+        ODocument od = result.get(0);
+        return new Usuario(od.field("nombre"), od.field("apellido"), od.field("email"), od.field("password"), od.field("username"), od.field("@rid").toString());
     }
     
-    private ODocument getUsuario(ODatabase db, String username) {
-        return (ODocument) db.query(new OSQLSynchQuery("SELECT FROM Usuario WHERE username=" + username)).get(0);
+    private ODocument getUsuario(ODatabase db, String rid) {
+        List<ODocument> result = db.command(new OSQLSynchQuery<ODocument>("SELECT FROM " + rid)).execute();
+        return result.get(0);
+    }
+
+    public List<Usuario> getAllUsuarios(String filtro, int pagina){
+        int skip = ((pagina * 10) - 10);
+        List<Usuario> ret = new ArrayList<>();
+        ODatabaseDocumentTx database = new ODatabaseDocumentTx("plocal:/databases/lab1nosql");
+        ODatabase db = database.activateOnCurrentThread().open("admin", "admin");
+        List<ODocument> list = database.command(new OSQLSynchQuery("SELECT FROM Usuario WHERE username LIKE ? LIMIT 10 SKIP ?")).execute("%" + filtro + "%", skip);
+        for(ODocument od : list){
+            ret.add(new Usuario(od.field("nombre"), od.field("apellido"), od.field("email"), od.field("password"), od.field("username"), od.field("@rid").toString()));
+        }
+        db.close();
+        return ret;
     }
 
     @Override
@@ -96,9 +118,9 @@ public class DALUsuario implements IDALUsuario {
         List<Usuario> ret = new ArrayList<>();
         ODatabaseDocumentTx database = new ODatabaseDocumentTx("plocal:/databases/lab1nosql");
         ODatabase db = database.activateOnCurrentThread().open("admin", "admin");
-        for (ODocument od : database.browseClass("Usaurio")) {
+        for (ODocument od : database.browseClass("Usuario")) {
             //ret.add(u.toJSON());
-            ret.add(new Usuario(od.field("nombre"), od.field("apellido"), od.field("email"), od.field("password"), od.field("rid")));
+            ret.add(new Usuario(od.field("nombre"), od.field("apellido"), od.field("email"), od.field("password"), od.field("username")));
         }
         db.close();
         return ret;
